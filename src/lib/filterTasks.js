42349273da148b8dbf629a58c0ccbf790f2908c5
tony <wangh9947@gmail.com>
2023-09-16 12:35:38 +0100

    filterTasks refactored

--- src/lib/filterTasks.js
+++ src/lib/filterTasks.js
@@ -1,14 +1,10 @@
 const filterTasks = (tasks, filterStatus) => {
+  if (filterStatus === "all") return tasks;
   const result = tasks.filter((task) => {
-    if (filterStatus === "incomplete") {
-      return task.completed === false;
-    }
-    if (filterStatus === "completed") {
-      return task.completed === true;
-    }
-    if (filterStatus === "all") {
-      return true;
-    }
+    return (
+      (filterStatus === "incomplete" && !task.completed) ||
+      (filterStatus === "completed" && task.completed)
+    );
   });
   return result;
 };
