6a56b044c4feb6ee03604e5eaabec6a64c3567f4
tony <wangh9947@gmail.com>
2023-09-16 12:43:16 +0100

    refactored filterTasksV2

--- src/lib/filterTasksV2.js
+++ src/lib/filterTasksV2.js
@@ -1,17 +1,7 @@
-const filterTasksV2 = (tasks, filterStatus, filterColors) => {
-  const filteredByStatus = tasks.filter((task) => {
-    if (filterStatus === "incomplete") {
-      return task.completed === false;
-    }
-    if (filterStatus === "completed") {
-      return task.completed === true;
-    }
-    if (filterStatus === "all") {
-      return true;
-    }
-  });
+import filterTasks from "./filterTasks";
 
-  return filteredByStatus.filter(
+const filterTasksV2 = (tasks, filterStatus, filterColors) => {
+  return filterTasks(tasks, filterStatus).filter(
     (task) => filterColors.includes(task.color) || filterColors.length === 0
   );
 };
