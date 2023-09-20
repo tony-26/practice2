416e2a51e42bed21f80b53bc5e8de2e1fd39d9ee
tony <wangh9947@gmail.com>
2023-09-20 19:08:33 +0100

    refactored filterTasks

--- src/lib/filterTasks.js
+++ src/lib/filterTasks.js
@@ -1,6 +1,5 @@
-const filterTasks = (tasks, filterStatus) => {
+export default (tasks, filterStatus) => {
   if (filterStatus === "all") return tasks;
   const status = filterStatus === "completed" ? true : false;
   return tasks.filter((task) => status === task.completed);
 };
-export default filterTasks;
--- src/lib/filterTasksV2.js
+++ src/lib/filterTasksV2.js
@@ -1,8 +1,8 @@
 import filterTasks from "./filterTasks";
 
-const filterTasksV2 = (tasks, filterStatus, filterColors) => {
-  return filterTasks(tasks, filterStatus).filter(
+const filterTasksV2 = (tasks, filterStatus, filterColors) =>
+  filterTasks(tasks, filterStatus).filter(
     (task) => filterColors.includes(task.color) || filterColors.length === 0
   );
-};
+
 export default filterTasksV2;
